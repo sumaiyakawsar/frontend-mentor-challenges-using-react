d10ae673e184b50543e0b53f73629b816b608e1b
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2025-06-08 23:04:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/frontend-mentor-challenges-using-react/",
-  plugins: [react()]
-
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning limit to 1000 kB (1 MB)
+  }
 })
